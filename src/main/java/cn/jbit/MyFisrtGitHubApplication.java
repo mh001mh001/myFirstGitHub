1feda1f6add30673704d8f4c25b927d39336b7a6
Administrator <Administrator@PC-WY027>
2021-07-06 23:09:41 +0800

    first1

---
+++ src/main/java/cn/jbit/MyFisrtGitHubApplication.java
@@ -0,0 +1,13 @@
+package cn.jbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFisrtGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFisrtGitHubApplication.class, args);
+	}
+
+}
